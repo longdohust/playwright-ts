e7933c2dcaf0fb42139728d215b2946da8b7592f
longdohust <longdo.hust@gmail.com>
2026-07-10 20:23:54 +0700

    [ID-27] Drag and drop handling

--- tests/web-elements/components/drag-and-drop.spec.ts
+++ tests/web-elements/components/drag-and-drop.spec.ts
@@ -2,9 +2,18 @@ import test, { expect, Page } from "@playwright/test";
 
 test('Verify select rating', async ({ page }) => {
     await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/components/drag-n-drop');
-    await dragAndDrop('Drag n Drop', ['Apple', ' Peach'], 'right', page)
-    await page.waitForTimeout(2000);
-
+    let inputs = ['Apple', 'Peach'];
+    await dragAndDrop('Drag n Drop', inputs, 'right', page);
+    let sourcePanelItems = await getPanelItemsByLabel('Drag n Drop', 'Source', page);
+    let targetPanelItems = await getPanelItemsByLabel('Drag n Drop', 'Target', page);
+    expect(isSubArray(sourcePanelItems, inputs)).toBeFalsy();
+    expect(isSubArray(targetPanelItems, inputs)).toBeTruthy();
+    inputs = ['Mango', 'Grapes'];
+    await dragAndDrop('Drag n Drop', inputs, 'left', page);
+    sourcePanelItems = await getPanelItemsByLabel('Drag n Drop', 'Source', page);
+    targetPanelItems = await getPanelItemsByLabel('Drag n Drop', 'Target', page);
+    expect(isSubArray(sourcePanelItems, inputs)).toBeTruthy();
+    expect(isSubArray(targetPanelItems, inputs)).toBeFalsy();
 
 });
 
@@ -21,3 +30,16 @@ async function dragAndDrop( label: string, inputs:string[], direction:'left' | '
         await sourcePanelLocator.locator(itemXpath).dragTo(targetPanelLocator);
     }
 };
+
+async function getPanelItemsByLabel(label: string, panel: 'Source' | 'Target', page: Page){
+    let spaceXpath = `(//span[contains(concat(' ', @class, ' '), 'ant-divider-inner-text') and normalize-space()='${label}']/following::div[contains(concat(' ', @class, ' '), ' ant-space ')])[1]`;
+    let spaceLocator = page.locator(spaceXpath);
+    let currentPanel = panel == 'Source' ? '.border-teal-500' : '.border-orange-500';
+    let panelLocator = spaceLocator.locator(currentPanel);
+    let items = await panelLocator.getByRole('button').allTextContents();
+    return items;
+}
+
+function isSubArray(parent: string[], subArray: string[]){
+    return subArray.every(el => parent.includes(el));
+}
